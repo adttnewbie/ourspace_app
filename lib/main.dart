2ffdf0bdcdd116923c979a5a867faa81f8490d02
adttnewbie <cuplyssss@gmail.com>
2026-07-29 15:18:11 +0700

    initials

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OurSpaceApp extends StatelessWidget {
+  const new({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
